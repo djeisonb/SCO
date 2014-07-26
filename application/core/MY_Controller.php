2d106fab3d30836a238c0c6ee1776dcc73ea6b99
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2014-07-26 19:28:36 -0300

    Funções Primarias do sistema
    PHP
    ==========
    * Configuração do **./../config/autoload.php**, setando as configurações
    principais
    * Formulação do **./../core/MY_Controller.php**, que irá gerenciar todos
    os subcontrollers
    * Formulação do **./../core/MY_Model.php**, que irá gerenciar todos os
    submodels
    * Definição de arquivo que será o template padrão
    (**./../views/template/default.php**)
    * Criação de função que irá salvar ou atualizar os dados passados pelos
    usuários em **./../core/MY_Model.php**

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,87 @@
+<?php
+    if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+    /**
+     * MY_Controller.php
+     * 
+     * @package     CI_Controller
+     * @subpackage  MY_Controller
+     * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+     * @abstract    Classe desenvolvida para gerenciar todas as outras subclasses
+     *              do sistema
+     */
+    class MY_Controller extends CI_Controller
+    {
+        /** 
+         * Variáveis protegidas usadas para definições de template, a view que 
+         * será usada, o título da página e a variável que transportará os dados
+         * para as views
+         **/
+        protected $template;
+        protected $view;
+        protected $titulo;
+        protected $dados;
+        
+        /**
+         * __construct()
+         * 
+         * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+         * @abstract    Realiza a construção da classe
+         * @param       bool $requer_autenticaco Se recebe TRUE, indica que para 
+         *              acessar o controller é necessário fazer login no sistema
+         * @access      Public
+         */
+        public function __construct($requer_autenticaco = TRUE)
+        {
+            parent::__construct($requer_autenticaco);
+            
+            session_start();
+            
+            /** Define o template padrao das páginas **/
+            $this->template = 'template/default';
+            $this->titulo   = 'SCO - Login';
+            
+            $this->verifica_login($requer_autenticaco);
+        }
+        //**********************************************************************
+        
+        /**
+         * LoadView()
+         * 
+         * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+         * @abstract    Função desenvolvida para carregar as visões e inserir os
+         *              dados
+         * @access      Public
+         */
+        public function LoadView()
+        {   
+            $this->dados['view']    = $this->view;
+            $this->dados['titulo']  = $this->titulo;
+            $this->load->view($this->template, $this->dados);
+        }
+        //**********************************************************************
+        
+        /**
+         * verifica_login()
+         * 
+         * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+         * @abstract    Função desenvolvida para verificar se o usuario está logado
+         * @access      Private
+         * @param       bool $requer_autenticacao indica se é necessário fazer 
+         *              login para acessar uma classe
+         */
+        private function verifica_login($requer_autenticacao)
+        {
+            if($requer_autenticacao)
+            {
+                if(!isset($_SESSION['admin']) || !isset($_SESSION['mobile']))
+                {
+                    redirect(app_baseurl().'login');
+                }
+            }
+        }
+        //**********************************************************************
+    }
+    
+    /** End of File MY_Controller.php **/
+    /** Location ./application/core/MY_Controller.php **/
---
+++ application/core/MY_Model.php
@@ -0,0 +1,99 @@
+<?php
+
+    /**
+     * MY_Model.php
+     * 
+     * @package     CI_Model
+     * @subpackage  MY_Model
+     * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+     * @abstract    Classe desenvolvida para gerenciar todos os outros submodels
+     *              do sistema
+     */
+    class MY_Model extends CI_Model
+    {
+        /**
+         * Definição das variáveis protegidas, onde serão setados as informações
+         * importantes para o banco de dados
+         **/
+        protected $BD;
+        protected $_tabela;
+        protected $_primaria;
+        
+        /**
+         * __construct()
+         * 
+         * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+         * @abstract    Realiza a construção da classe
+         * @access      Public
+         */
+        public function __construct()
+        {
+            parent::__construct();
+            
+            /** Faz o Load do banco de dados **/
+            $this->load->database('defalt', TRUE);
+        }
+        //**********************************************************************
+        
+        /**
+         * salvar()
+         * 
+         * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+         * @abstract    Função que verifica se os dados que estão sendo tratados
+         *              no momento serão salvos ou atualizados
+         * @access      Public
+         * @param       array $dados Contém os dados que serão passados ao banco
+         *              de dados
+         */
+        public function salvar($dados)
+        {
+            if(isset($this->_primaria) && !empty($this->_primaria))
+            {
+                return $this->update($dados);
+            }
+            else
+            {
+                return $this->insert($dados);
+            }
+        }
+        //**********************************************************************
+        
+        /**
+         * update()
+         * 
+         * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+         * @abstract    Função desenvolvida para realizar a atualização dos dados
+         * @access      Private
+         * @param       array $dados Contém os dados a serem atualizados
+         * @return      Retorna TRUE se atualizar e FALSE se não atualizar
+         */
+        private function update($dados)
+        {
+            $dados = array_map('mysql_escape_string', $dados);
+            
+            $this->BD->where($this->_primaria = $dados['id']);
+            
+            return $this->BD->update($this->_tabela, $dados);
+        }
+        //**********************************************************************
+        
+        /**
+         * insert()
+         * 
+         * @author      Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+         * @abstract    Função desenvolvida para salvar os dados
+         * @access      Private
+         * @param       array $dados Contém os dados que serão salvos
+         * @return      Retorna TRUE se salvar e FALSE se não salvar
+         */
+        function insert($dados)
+        {   
+            $dados = array_map('mysql_escape_string', $dados);
+            
+            return $this->BD->insert($this->_tabela, $dados);
+        }
+        //**********************************************************************
+    }
+    
+    /** End of File MY_Model.php **/
+    /** Location ./application/core/MY_Model.php **/
