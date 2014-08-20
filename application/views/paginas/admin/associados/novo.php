e122bc1141fa42205b8adaa178480ac878787f86
devLopez <fale_com_lopez@hotmail.com>
2014-08-19 23:18:29 -0300

    ![Bug Fix] - [x] Fixado BUG que impedia as textbox tomarem tamanho norma na visão *paginas/admin/associados/novo.php*

--- application/views/paginas/admin/associados/novo.php
+++ application/views/paginas/admin/associados/novo.php
@@ -271,7 +271,7 @@
                                             <input class="form-control" id="data_expiracao" type="text" placeholder="Data de expiração" required>
                                         </label>
                                         <span class="input-group-addon">
-                                        	<i class="fa fa-calendar"></i>
+                                        	<i class="fa fa-dollar"></i>
                                         </span>
                                     </div>
                                 </div>
@@ -281,11 +281,14 @@
                         <div class="row">
                             <section class="col col-12">
                                 <div class="form-group">
-                                    <label class="label"><strong>Parcelas:</strong></label>
+                                    <label class="label"><strong>Número de Mensalidades:</strong></label>
                                     <div class="input-group">
                                         <label class="input">
                                             <input class="form-control" id="data_expiracao" type="text" placeholder="Data de expiração" required>
                                         </label>
+                                        <span class="input-group-addon">
+                                        	<i class="fa fa-list-ol"></i>
+                                        </span>
                                     </div>
                                 </div>
                             </section>
@@ -299,19 +302,25 @@
                                         <label class="input">
                                             <input class="form-control" id="data_expiracao" type="text" placeholder="Data de expiração" required>
                                         </label>
+                                        <span class="input-group-addon">
+                                        	<i class="fa fa-calendar"></i>
+                                        </span>
                                     </div>
                                 </div>
                             </section>
                         </div>
                         
                         <div class="row">
-                            <section class="col col-6">
+                            <section class="col col-12">
                                 <div class="form-group">
-                                    <label class="label"><strong>Vencimento:</strong></label>
+                                    <label class="label"><strong>Dia do Vencimento:</strong></label>
                                     <div class="input-group">
                                         <label class="input">
                                             <input class="form-control" id="data_expiracao" type="text" placeholder="Data de expiração" required>
                                         </label>
+                                        <span class="input-group-addon">
+                                        	<i class="fa fa-thumb-tack"></i>
+                                        </span>
                                     </div>
                                 </div>
                             </section>
